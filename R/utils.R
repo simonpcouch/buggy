ff8867ff7fbf57c63b6a0d534d15a5400300f023
simonpcouch <simonpatrickcouch@gmail.com>
2025-03-19 12:12:26 -0500

    move unexported fns into separate file

---
+++ R/utils.R
@@ -0,0 +1,157 @@
+.thonk_env <- new_environment()
+
+extract_function_info <- function(call) {
+  if (is.null(call)) {
+    return(list(func = NULL, pkg = NULL))
+  }
+  
+  func_name <- as.character(call[[1]])
+  
+  # check if it's a namespaced call (pkg::func)
+  if (length(func_name) > 1 && grepl("::", func_name[1])) {
+    parts <- strsplit(func_name[1], "::")[[1]]
+    if (length(parts) == 2) {
+      pkg <- parts[1]
+      func <- parts[2]
+      
+      if (!pkg %in% c("rlang", "base")) {
+        return(list(func = func, pkg = pkg))
+      }
+    }
+  }
+  
+  # for non-namespaced calls
+  if (length(func_name) == 1) {
+    func <- func_name[1]
+    
+    tryCatch({
+      func_obj <- match.fun(func)
+
+      if (is_primitive(func_obj)) {
+        return(list(func = func, pkg = "base"))
+      }
+  
+      func_env <- environment(func_obj)
+      
+      if (isNamespace(func_env)) {
+        pkg <- environmentName(func_env)
+        return(list(func = func, pkg = pkg))
+      }
+    }, error = function(e) {})
+    
+    return(list(func = func, pkg = NULL))
+  }
+  
+  list(func = NULL, pkg = NULL)
+}
+
+
+extract_file_info <- function(back_trace) {
+  srcrefs <- back_trace$src
+  
+  for (i in seq_along(srcrefs)) {
+    if (!is.null(srcrefs[[i]])) {
+      file <- attr(srcrefs[[i]], "srcfile")$filename
+      if (!is.null(file) && file != "") {
+        line <- srcrefs[[i]][1]
+        return(list(file = file, line = line))
+      }
+    }
+  }
+  
+  if (rstudioapi::isAvailable()) {
+    context <- rstudioapi::getActiveDocumentContext()
+    if (!is.null(context$path) && context$path != "") {
+      cursor_pos <- context$selection[[1]]$range$start[1]
+      return(list(file = context$path, line = cursor_pos))
+    }
+  }
+  
+  list(file = NULL, line = NULL)
+}
+
+drop_thonk_handlers <- function() {
+  current_handlers <- globalCallingHandlers()
+  
+  if (is.null(current_handlers$error)) {
+    return(invisible(NULL))
+  }
+  
+  error_handlers <- current_handlers$error
+  handlers_to_keep <- list()
+  
+  if (is.function(error_handlers)) {
+    handler_env <- environment(error_handlers)
+    if (!rlang::env_has(handler_env, ".thonk_handler")) {
+      handlers_to_keep <- error_handlers
+    }
+  } else {
+    for (i in seq_along(error_handlers)) {
+      handler_env <- environment(error_handlers[[i]])
+      if (!rlang::env_has(handler_env, ".thonk_handler")) {
+        handlers_to_keep <- c(handlers_to_keep, list(error_handlers[[i]]))
+      }
+    }
+  }
+  
+  globalCallingHandlers(NULL)
+  
+  if (length(handlers_to_keep) > 0) {
+    if (length(handlers_to_keep) == 1 && !is.list(handlers_to_keep)) {
+      globalCallingHandlers(error = handlers_to_keep)
+    } else {
+      for (handler in handlers_to_keep) {
+        globalCallingHandlers(error = handler)
+      }
+    }
+  }
+  
+  invisible(NULL)
+}
+
+.stash_last_thonk <- function(x, which) {
+  if (!"pkg:thonk" %in% search()) {
+    do.call(
+      "attach",
+      list(new.env(), pos = length(search()), name = "pkg:thonk")
+    )
+  }
+  env <- as.environment("pkg:thonk")
+  env_bind(env, !!paste0(".last_thonk_", which) := x)
+  invisible(NULL)
+}
+
+set_thonk_chat <- function(x) {
+  if (is.null(x)) {
+    cli::cli_inform(
+      c(
+        "!" = "thonk requires configuring an ellmer Chat with the
+        {cli::col_blue('.thonk_chat')} option.",
+        "i" = "Set e.g.
+        {.code {cli::col_green('options(.thonk_chat = ellmer::chat_claude(model = \"claude-3-7-sonnet-latest\"))')}}
+        in your {.file ~/.Rprofile} and restart R."
+      ),
+      call = NULL
+    )
+    return(NULL)
+  }
+
+  if (!inherits(x, "Chat")) {
+    cli::cli_inform(
+      c(
+        "!" = "The option {cli::col_blue('.thonk_chat')} must be an ellmer
+        Chat object, not {.obj_type_friendly {x}}."
+      ),
+      call = NULL
+    )
+    return(NULL)
+  }
+
+  res <- x$set_turns(list())$clone()
+  env_bind(.thonk_env, chat = res)
+  res
+}
+
+get_thonk_chat <- function() {
+  env_get(.thonk_env, "chat")$clone()
+}
